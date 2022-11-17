d45693f1767baad83bb049751343933be1dd3baf
BishalP66 <110109666+BishalP66@users.noreply.github.com>
2022-11-17 22:21:40 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.131.31:9000/',
+  serverUrl: 'http://172.31.40.253:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
